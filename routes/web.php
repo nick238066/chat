16fb343ad17d3d01664dddf03d5ea13ecb90c5d0
Nick Hsieh <a1237255@gmail.com>
2020-05-07 23:04:25 +0800

    add chat-room

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/chat-room', 'ChatRoomController@index')->name('chat-room');
